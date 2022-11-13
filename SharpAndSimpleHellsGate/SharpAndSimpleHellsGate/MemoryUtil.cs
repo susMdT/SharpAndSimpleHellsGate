946ff4ef309451a58bfeee8c02bef3b0f4f7b70e
susMdT <68256613+susMdT@users.noreply.github.com>
2022-11-13 00:13:37 -0800

    Add files via upload

---
+++ SharpAndSimpleHellsGate/SharpAndSimpleHellsGate/MemoryUtil.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SharpAndSimpleHellsGate
+{
+    /// <summary>
+    /// Used to manipulate and extract information from a memory stream.
+    /// In this case the memory stream is the NTDLL module.
+    /// </summary>
+    public class MemoryUtil : IDisposable
+    {
+
+        /// <summary>
+        /// The memory stream representation of the NTDLL module.
+        /// </summary>
+        protected Stream ModuleStream { get; set; }
+
+        /// <summary>
+        /// Dispose the memory stream when no longer needed.
+        /// </summary>
+        ~MemoryUtil() => Dispose();
+
+        /// <summary>
+        /// Dispose the memory stream when no longer needed.
+        /// </summary>
+        public void Dispose()
+        {
+            this.ModuleStream.Dispose();
+            this.ModuleStream.Close();
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Extract a structure from the memory stream.
+        /// </summary>
+        /// <typeparam name="T">The Type of the structure to extract.</typeparam>
+        /// <param name="offset">The offset in the memory stream where the structure is located.</param>
+        /// <returns>The structure populated or the default structure.</returns>
+        protected T GetStructureFromBlob<T>(Int64 offset) where T : struct
+        {
+            byte[] bytes = this.GetStructureBytesFromOffset<T>(offset);
+            if (Marshal.SizeOf(typeof(T)) != bytes.Length)
+                return default;
+
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(T)));
+            Marshal.Copy(bytes, 0, ptr, bytes.Length);
+            T s = (T)Marshal.PtrToStructure(ptr, typeof(T));
+
+            Marshal.FreeHGlobal(ptr);
+            return s;
+        }
+
+        /// <summary>
+        /// Extract the code from a native Windows function.
+        /// </summary>
+        /// <param name="offset">The location of the function in the memory stream.</param>
+        /// <returns>The 24 bytes representing the code of the function.</returns>
+        protected byte[] GetFunctionOpCode(Int64 offset)
+        {
+            byte[] s = new byte[24];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, 24);
+            return s;
+        }
+
+        /// <summary>
+        /// Extract a DWORD value from the memory stream.
+        /// </summary>
+        /// <param name="offset">The location of the DWORD in the memory stream.</param>
+        /// <returns>The value of the DWORD.</returns>
+        protected UInt32 ReadPtr32(Int64 offset)
+        {
+            byte[] s = new byte[4];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, 4);
+            return BitConverter.ToUInt32(s, 0);
+        }
+
+        /// <summary>
+        /// Extract a QWORD value from the memory stream.
+        /// </summary>
+        /// <param name="offset">The location of the QWORD in the memory stream.</param>
+        /// <returns>The value of the QWORD.</returns>
+        protected UInt64 ReadPtr64(Int64 offset)
+        {
+            byte[] s = new byte[8];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, 8);
+            return BitConverter.ToUInt64(s, 0);
+        }
+
+        /// <summary>
+        /// Extract a WORD value from the memory stream.
+        /// </summary>
+        /// <param name="offset">The location of the WORD in the memory stream.</param>
+        /// <returns>The value of the WORD.</returns>
+        protected UInt16 ReadUShort(Int64 offset)
+        {
+            byte[] s = new byte[2];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, 2);
+            return BitConverter.ToUInt16(s, 0);
+        }
+
+        /// <summary>
+        /// Extract an ASCII string from the memory stream.
+        /// </summary>
+        /// <param name="offset">The location of the ASCII string in the memory stream.</param>
+        /// <returns>The ASCII string.</returns>
+        protected string ReadAscii(Int64 offset)
+        {
+            int length = 0;
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            while (this.ModuleStream.ReadByte() != 0x00) 
+                length++;
+
+            byte[] s = new byte[length];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, length);
+            return Encoding.UTF8.GetString(s);
+        }
+
+        /// <summary>
+        /// Extract the byte representation of a structure from the memory stream.
+        /// </summary>
+        /// <typeparam name="T">The Type of the structure to extract from the memory stream.</typeparam>
+        /// <param name="offset">The location of the structure in the memory stream.</param>
+        /// <returns>The structure as byte span.</returns>
+        protected byte[] GetStructureBytesFromOffset<T>(Int64 offset) where T : struct
+        {
+            byte[] s = new byte[Marshal.SizeOf(typeof(T))];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, s.Length);
+            return s;
+        }
+
+        /// <summary>
+        /// Get a specific amount of bytes at a specific location in the memory stream.
+        /// </summary>
+        /// <param name="offset">The location of the bytes to extract from the memory stream.</param>
+        /// <param name="size">The number of bytes to extract from the memory stream at a give location.</param>
+        /// <returns>The desired bytes as a byte span.</returns>
+        protected byte[] GetBytesFromOffset(Int64 offset, int size)
+        {
+            byte[] s = new byte[size];
+            this.ModuleStream.Seek(offset, SeekOrigin.Begin);
+            this.ModuleStream.Read(s, 0, size);
+            return s;
+        }
+    }
+}
---
+++ SharpAndSimpleHellsGate/SharpAndSimpleHellsGate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+namespace SharpAndSimpleHellsGate
+{
+    class Program
+    {
+        public static uint Gate() { return 5;  }
+        static void Main(string[] args)
+        {
+            IntPtr freeRWX = initializeRWX();
+            SystemModule ntdll = new SystemModule("ntdll.dll");
+            ntdll.LoadAllStructures();
+
+            IntPtr baseAddr = IntPtr.Zero;
+            IntPtr regionSize = (IntPtr)3000;
+            object[] allocArgs = new object[] { (IntPtr)(-1), baseAddr, IntPtr.Zero, regionSize, Macros.MEM_COMMIT , Macros.PAGE_EXECUTE_READWRITE};
+            Console.WriteLine($"Allocate Id is {ntdll.GetSyscallId("NtAllocateVirtualMemory")}");
+            Console.WriteLine("Ntstatus for Allocate: 0x{0:X}", (uint)directSyscall<Delegates.NtAllocateVirtualMemory>(ntdll.GetSyscallId("NtAllocateVirtualMemory"), freeRWX, allocArgs));
+            Console.WriteLine("Allocated to 0x{0:X}", (long)(IntPtr)allocArgs[1]);
+            baseAddr = (IntPtr)allocArgs[1];
+            regionSize = (IntPtr)allocArgs[3];
+
+            Console.WriteLine($"Protect Id is {ntdll.GetSyscallId("NtProtectVirtualMemory")}");
+            Console.WriteLine("Ntstatus for Protect: 0x{0:X}", (uint)directSyscall<Delegates.NtProtectVirtualMemory>(ntdll.GetSyscallId("NtProtectVirtualMemory"), freeRWX, new object[] { (IntPtr)(-1), baseAddr, regionSize, Macros.PAGE_EXECUTE_READ, (uint)0}));
+        }
+        public static IntPtr initializeRWX()
+        {
+            MethodInfo method = typeof(Program).GetMethod(nameof(Gate), BindingFlags.Static | BindingFlags.Public);
+            RuntimeHelpers.PrepareMethod(method.MethodHandle);
+
+            IntPtr pMethod = method.MethodHandle.GetFunctionPointer();
+            if (Marshal.ReadByte(pMethod) != 0xe9)
+            {
+                Console.WriteLine("Invalid Stub, the method table probably has the machine code instead of a JMP");
+                return pMethod;
+            }
+            Int32 offset = Marshal.ReadInt32(pMethod, 1);
+            UInt64 addr = (UInt64)pMethod + (UInt64)offset;
+            while (addr % 16 != 0) addr++;
+            return (IntPtr)addr;
+        }
+        public static object directSyscall<T>(short callId, IntPtr RWX, object[] args) where T : Delegate
+        { 
+            byte[] stub = new byte[] {
+                0x4c, 0x8b, 0xd1,                                      // mov  r10, rcx
+                0xb8, (byte)callId, (byte)(callId >> 8), 0x00, 0x00,   // mov  eax, <syscall
+                0x0f, 0x05,                                            // syscall
+                0xc3,                                                  // ret
+            };
+            Marshal.Copy(stub, 0, RWX, stub.Length);
+            return Marshal.GetDelegateForFunctionPointer(RWX, typeof(T)).DynamicInvoke(args);
+        }
+    }
+}
